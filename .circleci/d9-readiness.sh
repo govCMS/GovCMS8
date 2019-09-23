038a90aa617f146aad6a9fd0d3794f6cc12ae8be
sime <simon@hobbs.id.au>
2019-09-23 16:32:38 +1000

    Bug copying file.

--- .circleci/d9-readiness.sh
+++ .circleci/d9-readiness.sh
@@ -2,22 +2,22 @@
 IFS=$'\n\t'
 set -euo pipefail
 
-LOG=/app/d9-deprecations.log
+LOG=/app/d9-readiness.log
 
 composer require mglaman/drupal-check
 rm -f web/profiles/contrib/govcms/composer.json
 rm -f "${LOG}" && touch "${LOG}"
 
 set +e
-echo "DEPRECATION TEST FOR: GovCMS Profile"
-vendor/bin/drupal-check web/profiles/contrib/govcms > "${LOG}"
+echo echo -e "\nRUNNING DEPRECATION TEST FOR: GovCMS Profile"
+vendor/bin/drupal-check --no-progress web/profiles/contrib/govcms > "${LOG}"
 set -e
 
 for module in web/modules/contrib/*; do
     if [ -d "${module}" ]; then
-        echo "DEPRECATION TEST FOR: ${module}"
+        echo -e "\nRUNNING DEPRECATION TEST FOR: ${module}"
         set +e
-        vendor/bin/drupal-check "${module}" >> "${LOG}"
+        vendor/bin/drupal-check --no-progress "${module}" >> "${LOG}"
         set -e
     fi
 done
